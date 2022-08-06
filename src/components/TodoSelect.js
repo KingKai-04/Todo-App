481c6637441a7dcf8b3106cb1156194f8d683893
KingKai-04 <Laughingtoad417@gmail.com>
2022-08-06 10:48:05 -0400

    hello world was added

--- src/components/TodoSelect.js
+++ src/components/TodoSelect.js
@@ -2,21 +2,11 @@ import React, {useState} from 'react'
 
 const TodoSelect = ({todo, setTodos}) => {
 
-  const  [complete, setcomplete] = useState(false);
-
-  const handleDelete = ({id}) => {
-    setTodos(todo.filter((todo) => todo.id !== id))
-  }
-
   return (
     <div>
-      {todo.map((todo) => (
-        <li key={todo.id}>
-          <button className='delete task-button' onClick={handleDelete(todo)}>Clear All</button>
-          <button className='delete task-button' onClick={handleDelete(todo)}>Clear Completed</button>
-          <button className='delete task-button' onClick={handleDelete(todo)}>Clear Incomplete</button>
-        </li>
-        ))}
+      <p>
+        hello world
+      </p>
     </div>
   )
 }
